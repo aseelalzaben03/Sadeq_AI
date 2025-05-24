4e3b87d363c7d5ebf6bf4a7a62adcc8fd74b7598
aseelalzaben03 <167135023+aseelalzaben03@users.noreply.github.com>
2025-05-25 01:24:37 +0300

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,11 @@
 
+
+import re
+
+def clean_text(text):
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"<.*?>", "", text)
+    text = re.sub(r"\n", " ", text)
+    text = re.sub(r"\d+", "", text)
+    text = re.sub(r"[^\w\s]", "", text)
+    return text.strip()
